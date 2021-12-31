53ba354b625da5b357828737f9336927d4a3c728
Diego Plata <diegooplata@gmail.com>
2021-12-31 11:39:40 -0500

    Add links to navBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,67 @@
 import React from 'react';
 import styled from 'styled-components';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
   return (
     <Container>
-      <a href='/images/logo.svg'>
+      <a href='#'>
         <img src='/images/logo.svg' alt='' />
       </a>
-      <Menu>Test</Menu>
+      <Menu>
+        <a href='#'> Model S</a>
+
+        <a href='#'> Model 3</a>
+
+        <a href='#'> Model X</a>
+
+        <a href='#'> Model Y</a>
+      </Menu>
+      <RightMenu>
+        <a href='#'>Shop</a>
+        <a href='#'> Tesla Account</a>
+        <CustomMenu />
+      </RightMenu>
     </Container>
   );
 }
 
 export default Header;
 
-const Container = styled.div``;
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
 
-const Menu = styled.div``;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
