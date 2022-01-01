9d2a26749f93c2584fbda68251557c18a0efa492
Diego Plata <diegooplata@gmail.com>
2022-01-01 16:35:36 -0500

    Finish v1 visual

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
+  const changeBurgerStatus = () => {
+    setBurgerStatus(!burgerStatus);
+  };
+
   return (
     <Container>
       <a href='#'>
@@ -20,8 +27,43 @@ function Header() {
       <RightMenu>
         <a href='#'>Shop</a>
         <a href='#'> Tesla Account</a>
-        <CustomMenu />
+        <CustomMenu onClick={changeBurgerStatus} />
       </RightMenu>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={changeBurgerStatus} />
+        </CloseWrapper>
+        <li>
+          <a href='#'>Model S</a>
+        </li>
+        <li>
+          <a href='#'>Model 3</a>
+        </li>
+        <li>
+          <a href='#'>Model X</a>
+        </li>
+        <li>
+          <a href='#'>Model Y</a>
+        </li>
+        <li>
+          <a href='#'>Existing Inventory</a>
+        </li>
+        <li>
+          <a href='#'>Used Inventory</a>
+        </li>
+        <li>
+          <a href='#'>Trade-in</a>
+        </li>
+        <li>
+          <a href='#'>Cybertruck</a>
+        </li>
+        <li>
+          <a href='#'>Roadster</a>
+        </li>
+        <li>
+          <a href='#'>Semi</a>
+        </li>
+      </BurgerNav>
     </Container>
   );
 }
@@ -33,10 +75,12 @@ const Container = styled.div`
   position: fixed;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 20px;
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `;
 const Menu = styled.div`
   display: flex;
@@ -49,6 +93,10 @@ const Menu = styled.div`
     text-transform: uppercase;
     padding: 0 10px;
     flex-wrap: nowrap;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
   }
 `;
 
@@ -64,4 +112,36 @@ const RightMenu = styled.div`
 
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${props => (props.show ? 'translateX(100%)' : 'translateX(0)')};
+  transition: transform 0.2s;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  }
+  a {
+    font-weight: 600;
+  }
+`;
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
